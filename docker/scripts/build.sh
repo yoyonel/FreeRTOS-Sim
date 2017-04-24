d1f265727098e805b386b50d2d0b3fc96bde6184
yoyonel <yoyonel@hotmail.com>
2017-04-24 16:45:42 +0200

    [Docker] Update

--- docker/scripts/build.sh
+++ docker/scripts/build.sh
@@ -2,16 +2,27 @@
 
 set -e
 
+# Path to find 'FreeRTOSConfig.h'
+# pass from the host to container (it's a data)
 export FREERTOS_CONFIG=/FreeRTOS-Sim/Project
 
+# Go to sources directory (Kernel+Simulator for FreeRTOS linux port)
+# (Host mount volume)
 cd /FreeRTOS-Sim
 
+# Link with source for (current) project
+# (Host mount volume)
 ln -s /FreeRTOS-Project /FreeRTOS-Sim/Project
 
+# Go to build directory
+# (Host mount volume)
 cd /FreeRTOS-Build
 
+# Configure the project with CMake (using CMakeLists.txt present in: /FreeRTOS-Sim directory)
 cmake . /FreeRTOS-Sim
 
+# Build the project
 make -j
 
+# Remove symbolic link (clean this residual file in Host mounted volume: /FreeRTOS-Sim)
 rm -f /FreeRTOS-Sim/Project
